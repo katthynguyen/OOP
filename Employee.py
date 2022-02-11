41f5835eadcb10ba2c1ae797e7f07ea39bdd9869
thanhntk8 <thanhntk8@fpt.com.vn>
2022-02-11 14:52:54 +0700

    decorator setter, deleter

--- Employee.py
+++ Employee.py
@@ -7,12 +7,30 @@ class Employee:
         self.first = first
         self.last = last
         self.pay = pay
+        
+        # self.email = first + '.'+last+ str(symble)+'@email.com'
+
+    @property
+    def email(self):
         symble = randrange(100)
-        self.email = first + '.'+last+ str(symble)+'@email.com'
-    
+        return '{}{}{}{}'.format(self.first,self.last,symble,'@fpt.com.vn')
+
+    @property    
     def fullname(self):
         return '{}'.format(self.first)+' '+'{}'.format(self.last)+ ' ' + '{}'.format(self.pay)
     
+    @fullname.setter
+    def fullname(self,name):
+        first,last = name.split(' ')
+        self.first = first
+        self.last = last
+
+    @fullname.deleter
+    def fullname(self):
+        print('Delete Name')
+        self.first = None
+        self.last = None
+
     @classmethod
     def full_more(cls, str):
         if len(str) < 3:
@@ -24,21 +42,25 @@ class Employee:
         return "Emplyee("'{}'.format(self.first)+' '+'{}'.format(self.last)+ ' ' + '{}'.format(self.pay) +")"
 emp_1 = Employee('kat','nguyen',50)
 # print(emp_1.full_more(emp_1.fullname()))
-# print()
+# print(emp_1.fullname)
+# print(emp_1.email)
 
+emp_1.fullname = 'Katthy nguyen'
+print(emp_1.fullname)
+del emp_1.fullname
 # inheritated
-class Manager(Employee):
-    def __init__(self, first, last, pay,like):
-        # super.__init__(first, last, pay)        
-        Employee.__init__(self,first,last,pay)
-        self.like = like
+# class Manager(Employee):
+#     def __init__(self, first, last, pay,like):
+#         # super.__init__(first, last, pay)        
+#         Employee.__init__(self,first,last,pay)
+#         self.like = like
 
 
-# print(help(Manager))
-ma = Manager('ji','hin',60,56)
-# print(ma.email)
-# print(ma.fullname())
-# print(ma.__dict__)
-# print(ma.like)
+# # print(help(Manager))
+# ma = Manager('ji','hin',60,56)
+# # print(ma.email)
+# # print(ma.fullname())
+# # print(ma.__dict__)
+# # print(ma.like)
 
-print(emp_1)
+# print(emp_1)
